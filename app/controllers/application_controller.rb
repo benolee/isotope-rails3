6cf916756c56b8b0539239dd5bcf222262eed146
Ben Holley <ben@isotope11.com>
2010-11-10 12:19:10 -0600

    [CONTROLLERS] remove unnecessary methods from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,6 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  filter_parameter_logging :password, :password_confirmation
-  helper_method :current_user_session, :current_user
 
   private
 
