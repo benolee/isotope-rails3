ac9759369a06c12affb621ee05ac159bd33f72ce
Ben Holley <ben@isotope11.com>
2010-11-10 13:50:58 -0600

    [ARCH] removed unnecessary files, added tab_tab plugin, added .swp to gitignore, modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ IsotopeRails3::Application.routes.draw do
   devise_for :users
 
   root :to => 'home#index'
+  match '/home/about' => 'home#about'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
