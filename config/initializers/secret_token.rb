2f48e0c5f5c1bf458fcae9f2c40ee6a91fb26b4d
rclements <rclements@isotope11.com>
2010-11-10 09:59:37 -0600

    [INITIAL] initial commit with updated README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IsotopeRails3::Application.config.secret_token = '30b41392351022a6b681709aea3420d7d42088568b2c491ad3514bbc4193b9da725bfb237bb783e50901148764ee162c68772f1169c56b47bd2f4ad85b1dafbc'
