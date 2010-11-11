077eb9f887338d0221f18750be31892c4729c206
Ben Holley <ben@isotope11.com>
2010-11-11 09:29:45 -0600

    [DB] add avatar to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101110222023) do
+ActiveRecord::Schema.define(:version => 20101110225138) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -40,6 +40,10 @@ ActiveRecord::Schema.define(:version => 20101110222023) do
     t.string   "last_name"
     t.string   "position"
     t.text     "bio"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.string   "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
