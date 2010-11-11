4fce9c30d1150ce9fb14482175a238d7e4fa4e22
Ben Holley <ben@isotope11.com>
2010-11-11 13:53:02 -0600

    [ROUTES] specify devise paths for site login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 IsotopeRails3::Application.routes.draw do
   root :to => 'home#index'
 
-  devise_for :users
+  devise_for :users, :path => '/', :path_names => { :sign_in => 'login', :sign_out => 'logout' }
 
   resources :pages, :controller => 'high_voltage/pages', :only => [:show]
   resources :posts
