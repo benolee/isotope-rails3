9130213a29eea0716ee5f0c96dad11083a21abc4
Ben Holley <ben@isotope11.com>
2010-11-11 10:34:16 -0600

    [BLOG] add users to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101110225138) do
+ActiveRecord::Schema.define(:version => 20101111155615) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
