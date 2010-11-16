2e82676197cd9c42373cdfa75cc26ff7067b6080
Adam Dill <altrux@gmail.com>
2010-11-16 13:22:36 -0600

    [DEPLOY] add restart to server

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,17 +67,10 @@ after 'deploy:update_code', 'deploy:symlink_shared'
 # after 'deploy:update_code', 'bundler:symlink_bundled_gems'
 # after 'deploy:update_code', 'bundler:install'
 
-
-
-
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
 # end
