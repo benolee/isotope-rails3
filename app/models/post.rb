68d189c53cf349aa5df7e651a5f24d0dc7c5ccee
Ben Holley <ben@isotope11.com>
2010-11-16 10:54:52 -0600

    [MODELS] add validations to User and Post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :title, :body, :user_id
+  # TODO validate title is all characters
+  validates_uniqueness_of :title
   scope :for_user, lambda{ |user| where('user_id = ?', user.id)}
 end
