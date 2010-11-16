68c826dcb7b4dc69d359bb578e2441b901f72c93
Adam Dill <altrux@gmail.com>
2010-11-16 13:24:31 -0600

    [DEPLOY] remove redundant code

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,11 +66,3 @@ after 'deploy:update_code', 'deploy:symlink_shared'
 # 
 # after 'deploy:update_code', 'bundler:symlink_bundled_gems'
 # after 'deploy:update_code', 'bundler:install'
-
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-# end
