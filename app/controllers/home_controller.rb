bf46fa99b31d99cc5ddd49e880ad361a0856dfb1
Adam Dill <altrux@gmail.com>
2010-11-16 16:51:09 -0600

    [HOME] remove old actions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -34,46 +34,6 @@ class HomeController < ApplicationController
     tab :services
   end
 
-  def sub
-  end
-
-  def work
-    tab :work
-  end
-
-  def work_acolleague
-  end
-
-  def work_aidt
-  end
-
-  def work_aiua
-  end
-
-  def work_creative_catering
-  end
-
-  def work_deep_south_fibers
-  end
-
-  def work_insight
-  end
-
-  def work_in_the_making
-  end
-
-  def work_luvnotes
-  end
-
-  def work_maralyn_wilson
-  end
-
-  def work_political_inquirer
-  end
-
-  def work_sportgraphics
-  end
-
   def careers
   end
 
