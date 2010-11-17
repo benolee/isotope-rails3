93d2170e59da072e247580acec63926bfd9225b0
Adam Dill <altrux@gmail.com>
2010-11-17 16:19:54 -0600

    [MAILER] tweaked mailer settings

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ActionMailer::Base
-  default :from => "contact@isotope11.com"
+  default :from => "do_not_reply@isotope11.com"
 
   def contact_email(contact_form)
     @contact_form = contact_form
     mail(
-      :to => ["josh@isotope11.com", "laird@isotope11.com"],
-      :subject => "Somebody used the contact form!"
+      :to => "info@isotope11.com",
+      :subject => "[WEBSITE] Contact Form"
     )
   end
 end
